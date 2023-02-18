0c3957427de163786178a0fba16518fd95d54c77
fAndreuzzi <andreuzzi.francesco@gmail.com>
2023-02-18 17:48:18 +0100

    plot multiple distributions

--- plot_g.py
+++ plot_g.py
@@ -1,14 +1,25 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-data = np.fromregex('fort.9', r"\s*([^\s]+)\s+([^\s]+)", [('rad', float), ('g', float)])
-r = data['rad']
-g = data['g']
+def extract_data(filename):
+    return np.fromregex(filename, r"\s*([^\s]+)\s+([^\s]+)", [('rad', float), ('g', float)])
+
+files = [
+    # (filename, label)
+]
+
 plt.figure(figsize=(20,5))
+for filename, label in files:
+    data = extract_data(filename)
+    r = data['rad']
+    g = data['g']
 
-plt.plot(r,g)
+    plt.plot(r, g, label=label)
+
 plt.grid()
-
 plt.title('g(r)')
+
+if len(files) > 1:
+    plt.legend()
 
 plt.show()
