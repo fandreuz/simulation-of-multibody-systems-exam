124326b8263ce16291514b67a6301f90bc8bbcd6
fAndreuzzi <andreuzzi.francesco@gmail.com>
2023-02-19 21:02:53 +0100

    fixes

--- plot_g.py
+++ plot_g.py
@@ -1,4 +1,9 @@
 import matplotlib.pyplot as plt
+import matplotlib
+font = {'weight' : 'bold',
+        'size'   : 20}
+matplotlib.rc('font', **font)
+
 import numpy as np
 
 def extract_data(filename):
@@ -8,18 +13,23 @@ files = [
     # (filename, label)
 ]
 
-plt.figure(figsize=(20,5))
+plt.figure()
+
 for filename, label in files:
     data = extract_data(filename)
     r = data['rad']
     g = data['g']
 
-    plt.plot(r, g, label=label)
+    r = r.reshape((100,-1), order="F")
+    g = g.reshape((100,-1), order="F")
+    plt.plot(r[:,0], g[:,-1], label=f"T={label}")
 
+plt.legend()
 plt.grid()
-plt.title('g(r)')
+plt.title("g(r)")
 
-if len(files) > 1:
-    plt.legend()
+plt.subplots_adjust(top = 1, bottom = 0, right = 1, left = 0,
+            hspace = 0, wspace = 0)
+plt.margins(0,0)
 
-plt.show()
+plt.savefig("g.pdf", bbox_inches = 'tight', pad_inches = 0, dpi=300)
