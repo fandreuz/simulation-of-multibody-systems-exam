f552cb0efca8fce3fa6a17309429709302891e69
fAndreuzzi <andreuzzi.francesco@gmail.com>
2023-02-21 00:12:35 +0100

    __main__

--- extract_positions.py
+++ extract_positions.py
@@ -1,4 +1,5 @@
 import numpy as np
+import sys
 
 def extract(filename):
     with open(filename) as f:
@@ -12,3 +13,10 @@ def extract(filename):
     data = data[:len(data) // 2]
 
     return np.array(tuple(map(float, data))).reshape(3, -1, order='F')
+
+def extract_to(filename, dest):
+    data = extract(filename)
+    np.savetxt(dest, data.T, fmt="%12.8f")
+
+if __name__ == "__main__":
+    extract_to(sys.argv[1], sys.argv[2])
