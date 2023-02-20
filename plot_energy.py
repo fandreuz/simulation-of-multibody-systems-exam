f9381d15fc4c5f8993811de7afd23097b0976796
fAndreuzzi <andreuzzi.francesco@gmail.com>
2023-02-20 08:48:01 +0100

    split

--- plot_energy.py
+++ plot_energy.py
@@ -11,22 +11,29 @@ kinetic = data['kinetic']
 potential = data['potential']
 total = data['total']
 
-plt.figure()
+fig = plt.figure()
+plt.subplots_adjust(top = 1, bottom = 0, right = 1, left = 0,
+                    wspace=0.25,
+                    hspace=0.25)
 
 dt = 0.004
 x = dt * np.arange(1,len(kinetic)+1)
 
-plt.plot(x, kinetic, label='Kinetic')
+plt.subplot(2,1,1)
+plt.plot(x, kinetic)
+
+plt.title('Kinetic energy')
+
+plt.grid()
+
+plt.subplot(2,1,2)
 plt.plot(x, potential, label='Potential')
 plt.plot(x, total, label='Total')
 
+plt.title('Potential/total energy')
+
 plt.xlabel("Time")
 plt.grid()
-plt.title('Energy')
 plt.legend()
-
-plt.subplots_adjust(top = 1, bottom = 0, right = 1, left = 0,
-            hspace = 0, wspace = 0)
-plt.margins(0,0)
 
 plt.savefig(f"energy.pdf", bbox_inches = 'tight', pad_inches = 0, dpi=300)
